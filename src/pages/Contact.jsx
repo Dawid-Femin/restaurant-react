eed5faa8e28375e17c081d6419dffc5b4445f331
Dawid Femin <dawid.femin@gmail.com>
2023-02-10 02:08:14 +0100

    Created Contact view

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../components/Footer.jsx';
 import Header from '../components/Header.jsx';
 import Navbar from '../components/Navbar.jsx';
+import '../style/Contact.css';
 
 const Contact = () => {
+  useEffect(() => {
+    const iFrameData=document.getElementById('iFrameID');
+    const latitude = 50.03386542862901;
+    const longitude = 22.006992770986663;
+    iFrameData.src=`https://maps.google.com/maps?q=${latitude},${longitude}&hl=es;&output=embed`
+  })
   return (
     <>
     <Navbar />
     <Header />
-    <h2>Contact</h2>
+    <div className="contact-container">
+      <div className="contact-section">
+        <div className="left-info">
+          <h3>Informacje kontaktowe</h3>
+          <p>Warszawa, PL</p>
+          <p>ul. Mazowiecka 14</p>
+          <p>Telefon: +48 888 999 777</p>
+          <p>Godziny otwarcia:</p>
+          <p>Pn.-Pt.: 11:30 - 22:00</p>
+          <p>Sb.-Nd.: 14:30 - 23:30</p>
+        </div>
+        <div className="right-info">
+          <h3>Formularz kontaktowy</h3>
+          <form action="POST" title='Formularz kontaktowy'>
+            <input type="text" placeholder='Imię i nazwisko' />
+            <input type="text" placeholder='Adres e-mail' />
+            <textarea placeholder='Wiadomość' rows="4" cols="50"></textarea>
+            <button>Wyślij wiadomość</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div className="map">
+      <iframe id='iFrameID' height='500px' width='100%'></iframe>
+    </div>
     <Footer />
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import Navbar from '../components/Navbar.jsx';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
-import AboutUs from '../components/AboutUs.jsx';
+import PizzaPhoto from '../assets/restaurant-photo.jpg';
+import '../style/Home.css';
+
 
 const Home = () => {
   return (
     <>
     <Navbar />
     <Header />
-    <AboutUs />
+    <div className="about-us-container">
+      <div className="about-us-sections">
+        <div className="about-us-left">
+        <h3>Kilka słów o nas ...</h3>
+          <p>Restauracja specjalizuje się w przygotowywaniu tradycyjnych włoskich pizz, które są przygotowywane z najlepszych składników i pieczone w specjalnym piecu na drewno. W menu znajdują się różne rodzaje pizz, od klasycznych smaków takich jak margherita, po bardziej wykwintne opcje z dodatkami takimi jak świeże owoce i wędliny. Klienci mogą także zamówić pizze na miejscu lub zabrać je do domu dzięki opcji dostawy. Restauracja posiada przytulne i kameralne wnętrze, w którym goście mogą cieszyć się swoimi posiłkami w spokojnej atmosferze. Obsługa jest miła i profesjonalna, a kucharze są gotowi doradzić w wyborze najlepszej pizzy dla każdego gościa.</p>
+        </div>
+        <div className="about-us-right">
+          <img src={PizzaPhoto} alt="pizza" />
+        </div>
+      </div>
+    </div>
     <Footer />
     </>
   )
